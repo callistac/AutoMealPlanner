9f99ac8fe36c0bca3dd6e9e4c429d938858bc835
Callista Christ <callistac@cs.uchicago.edu>
2020-01-26 21:52:00 -0600

    added upded user class and SQL file

--- User_Class.py
+++ User_Class.py
@@ -1,14 +1,15 @@
 
 
 class User(object):
-    def __init__(self, name, zipcode, highly_rated):
+    def __init__(self, name, zipcode, allergies, meal_types, weekly_budget):
         self.name = name
         self.zipcode = str(zipcode)
         self.highly_rated = []
-        self.allergies = []
+        self.allergies = allergies
         self.cuisines = []
         self.meal_types = []
         self.items_in_kitchen = []
+        self.weekly_budget = weekly_budget
 
     # records highly rated recipes that the user likes each week
     def get_highly_rated_recipes(self, recipe_link, rating):
