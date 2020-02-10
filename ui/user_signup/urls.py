c337824148ad114523a686103a4aa2b81a915c80
Callista Christ <callistac@cs.uchicago.edu>
2020-02-10 00:05:04 -0600

    added interface for user to input zipcode, budget, etc.

--- ui/user_signup/urls.py
+++ ui/user_signup/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='user_signup/logout.html')),
     path('login/new_user/', views.register, name='user_signup/new_user.html'),
     path('about/', views.about, name='user_signup/about.html'),
-    path('about/.', views.about_redirect, name = 'about_redirect')
-
-
-
+    path('about/.', views.about_redirect, name = 'about_redirect'),
+    path('login/new_user/user_info', views.user_info, name='user_info')
 ]
--- ui/user_signup/views.py
+++ ui/user_signup/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def user_signup(request):
@@ -14,6 +15,9 @@ def new_user(request):
 def about(request):
     return render(request, 'user_signup/about.html', {})
 
+def user_info(request):
+    return render(request, 'user_signup/user_information.html', {})
+
 def about_redirect(request):
     return redirect('/home/about')
 
@@ -27,3 +31,8 @@ def register(request):
         form = UserCreationForm()
         args = {"form":form}
         return render(request, 'user_signup/new_user.html', args)
+
+
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'user_signup/home.html')
