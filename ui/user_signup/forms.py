f44137b06435a11e5b74b6ed1c4bcb7589c749da
kgozman6159 <34196425+kgozman6159@users.noreply.github.com>
2020-02-19 17:24:34 -0600

    migrations are being silly

--- ui/user_signup/forms.py
+++ ui/user_signup/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
-from user_signup.models import User_Data_Posts
+from user_signup.models import User_Data
 
 class CustomForm(forms.ModelForm):
     post = forms.CharField()
     class Meta:
-        model = User_Data_Posts
+        model = User_Data
         fields = ('post',)
--- ui/user_signup/models.py
+++ ui/user_signup/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 # Create your models here.
-class User_Data_Posts(models.Model):
+class User_Data(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
-    post = models.CharField(max_length=500, default='')
+    firstname = models.CharField(max_length=500, default='')
+    lastname = models.CharField(max_length=500, default='')
+    email = models.EmailField(max_length=500, default='')
+    zip = models.IntegerField(default=0)
+    budget = models.CharField(max_length=500, default='')
+    laziness = models.IntegerField(default=0)
