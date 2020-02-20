95ceb070c7f6ceca3a144680ee6aeea1df0964e2
kgozman6159 <34196425+kgozman6159@users.noreply.github.com>
2020-02-19 23:42:17 -0600

    got redirects and dropdown menus

--- ui/user_signup/models.py
+++ ui/user_signup/models.py
@@ -1,13 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+
 
 # Create your models here.
 class User_Data(models.Model):
     #user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    BUDGET_CHOICES = [
+        ('<$40', '<$40'),
+        ('$40-60', '$40-60'),
+        ('$60-80', '$60-80'),
+        ('$80-100', '$80-100'),
+        ('>$100', '>$100')
+    ]
+
+    LAZINESS_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('3','3'),
+    ('4', '4'),
+    ('5','5')
+    ]
+
     firstname = models.CharField(max_length=500, default='')
     lastname = models.CharField(max_length=500, default='')
     email = models.EmailField(max_length=500, default='')
-    zip = models.IntegerField(default=0)
-    budget = models.CharField(max_length=500, default='')
-    laziness = models.IntegerField(default=0)
+    zip = models.PositiveIntegerField()
+    budget = models.CharField(max_length=500, default='', choices = BUDGET_CHOICES)
+    laziness = models.CharField(max_length = 50, choices = LAZINESS_CHOICES)
+
+    def get_absolute_url(self):
+        #return HttpResponseRedirect(reverse('login'))
+        return reverse('login')
--- ui/user_signup/urls.py
+++ ui/user_signup/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='user_signup/home.html'),
-    path('login/', LoginView.as_view(template_name='user_signup/user_signup.html')),
+    path('login/', LoginView.as_view(template_name='user_signup/user_signup.html'), name = 'login'),
     path('logout/', LogoutView.as_view(template_name='user_signup/logout.html')),
     path('login/new_user/', views.register, name='user_signup/new_user.html'),
     path('about/', views.about, name='user_signup/about.html'),
