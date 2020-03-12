ca6be666fd43f228c8cf617cdc55b7708a4d8aa5
kgozman6159 <34196425+kgozman6159@users.noreply.github.com>
2020-03-12 18:23:35 -0500

    add price kinda

--- ui/user_signup/make_grocery_list.py
+++ ui/user_signup/make_grocery_list.py
@@ -44,6 +44,7 @@ def make_grocery_list(ingredient_list):
         amount = ingredient[1]
         unit = ingredient[2]
         name = ingredient[3]
+        recipe_num = ingredient[4]
         if unit in TINY_UNITS:
             amount *= 0.01
             unit = "ounce"
@@ -64,7 +65,7 @@ def make_grocery_list(ingredient_list):
 
         if name not in ingredients.keys():
             ingredients[name] = {"servings": servings, "amount": amount, \
-                    "unit": unit}
+                    "unit": unit, "recipe_num" : recipe_num}
 
         elif ingredients[name]["unit"] == unit:
             ingredients[name]["amount"] += amount
